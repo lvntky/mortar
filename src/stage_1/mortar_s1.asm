45936c523d55efca358f9270f97e690775b1bf9b
lvntky <klevent1903@gmail.com>
2024-07-07 23:50:57 +0300

    [feature] start up message

--- src/stage_1/mortar_s1.asm
+++ src/stage_1/mortar_s1.asm
@@ -1,7 +1,30 @@
-[BITS 16]
-[ORG 0x7C00]
+BITS 16
+ORG 0x7C00
 
 
+
+start:
+        ; Clear the screen
+        mov ah, 0x00      ; AH = 0x00 (Video Services)
+        mov al, 0x03      ; AL = 0x03 (Clear screen)
+        int 0x10          ; BIOS interrupt for video services
+        mov bx, message
+repeat:
+        mov al, [bx]
+        test al, al
+        je hang
+        push bx
+        mov ah, 0x0e
+        mov bx, 0x0004
+        int 0x10
+        pop bx
+        inc bx
+        jmp repeat
+
+hang:
+        jmp $
+
+message: db "Mortar Bootloader VERSION 0.0.1", 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
